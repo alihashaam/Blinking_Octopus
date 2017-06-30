04ed1e0879e08e3ccea5c7366036ed88eb49140b
Urmikl18 <urmikl@hotmail.com>
2017-06-30 11:41:13 +0200

    Added doc and jar for SF10

--- blinktopus/src/main/java/dbse/fopj/blinktopus/test/Evaluation.java
+++ blinktopus/src/main/java/dbse/fopj/blinktopus/test/Evaluation.java
@@ -44,6 +44,7 @@ public class Evaluation {
 		double averageRow = rowTimes.stream().mapToLong(Long::longValue).sum() / nQuery;
 		double averageCol = colTimes.stream().mapToLong(Long::longValue).sum() / nQuery;
 		
+		System.out.println("\n!!!! Result: LOG: "+averageLog+" ; ROW: "+averageRow+" ; COL: "+averageCol+" !!!\n");
 		double[] res = {averageLog, averageRow, averageCol, 0};
 		return res;
 	}
@@ -93,6 +94,7 @@ public class Evaluation {
 		double averageCol = colTimes.stream().mapToLong(Long::longValue).sum() / nQuery;
 		double averageAQP = aqpTimes.stream().mapToLong(Long::longValue).sum() / nQuery;
 		
+		System.out.println("\n!!!! Result: LOG: "+averageLog+" ; ROW: "+averageRow+" ; COL: "+averageCol+" ; AQP: "+averageAQP+" !!!\n");
 		double[] res = {averageLog, averageRow, averageCol, averageAQP};
 		return res;
 	}
@@ -142,6 +144,8 @@ public class Evaluation {
 		double averageCol = colTimes.stream().mapToLong(Long::longValue).sum() / nQuery;
 		double averageAQP = aqpTimes.stream().mapToLong(Long::longValue).sum() / nQuery;
 		
+		
+		System.out.println("\n!!!! Result: LOG: "+averageLog+" ; ROW: "+averageRow+" ; COL: "+averageCol+" ; AQP: "+averageAQP+" !!!\n");
 		double[] res = {averageLog, averageRow, averageCol, averageAQP};
 		return res;
 	}
@@ -195,26 +199,26 @@ public class Evaluation {
 //		String O_2 = "O_0.5.tbl";
 //		String LI_2 = "LI_0.5.tbl";
 		
-		String O_1 = "O_1.tbl";
-		String LI_1 = "LI_1.tbl";
+//		String O_1 = "O_1.tbl";
+//		String LI_1 = "LI_1.tbl";
 //		
 		String O_2 = "O_10.tbl";
 		String LI_2 = "LI_10.tbl";
 		
 		String table = "orders";
 		String attr = "totalprice";
-		
-		double lower1_10 = 800;
-		double higher1_10 = 36611;
-		
-		double lower1_90 = 800;
-		double higher1_90 = 257744;
-		
-		double lower2_10 = 800;
-		double higher2_10 = 37512;
-		
-		double lower2_90 = 800;
-		double higher2_90 = 264458;
+//		
+//		double lower1_10 = 800;
+//		double higher1_10 = 36611;
+//		
+//		double lower1_90 = 800;
+//		double higher1_90 = 257744;
+//		
+//		double lower2_10 = 800;
+//		double higher2_10 = 37512;
+//		
+//		double lower2_90 = 800;
+//		double higher2_90 = 264458;
 		
 //		double lower1_10 = 800;
 //		double higher1_10 = 38769;
@@ -222,55 +226,66 @@ public class Evaluation {
 //		double lower1_90 = 800;
 //		double higher1_90 = 273702;
 //		
-//		double lower2_10 = 800;
-//		double higher2_10 = 40000;
+		double lower2_10 = 800;
+		double higher2_10 = 40000;
 //		
-//		double lower2_90 = 800;
-//		double higher2_90 = 280000;
+		double lower2_90 = 800;
+		double higher2_90 = 280000;
 		
 		int nQuery = 100;
 		
-		System.out.println("Start SF1");
-		loadData(O_1,LI_1);
-		System.out.println("\tStart Experiment 1");
-		System.out.println("\t\tStart 10%");
-		ColSV col = new ColSV("Col3", table, attr, lower1_10, higher1_10);
-		RowSV row = new RowSV("Row4", table, attr, lower1_10, higher1_10);
-		double[] res1_1 = experiment1(col, row, table, attr, lower1_10, higher1_10, nQuery);
-		System.out.println("\t\tStart 90%");
-		col = new ColSV("Col3", table, attr, lower1_90, higher1_90);
-		row = new RowSV("Row4", table, attr, lower1_90, higher1_90);
-		double[] res1_2 = experiment1(col, row, table, attr, lower1_90, higher1_90, nQuery);
+//		System.out.println("Start SF1");
+//		loadData(O_1,LI_1);
+//		System.out.println("\tStart Experiment 1");
+//		System.out.println("\t\tStart 10%");
+//		ColSV col = new ColSV("Col3", table, attr, lower1_10, higher1_10);
+//		RowSV row = new RowSV("Row4", table, attr, lower1_10, higher1_10);
+//		double[] res1_1 = experiment1(col, row, table, attr, lower1_10, higher1_10, nQuery);
+//		System.out.println("\t\tStart 90%");
+//		col = new ColSV("Col3", table, attr, lower1_90, higher1_90);
+//		row = new RowSV("Row4", table, attr, lower1_90, higher1_90);
+//		double[] res1_2 = experiment1(col, row, table, attr, lower1_90, higher1_90, nQuery);
 		
 		System.out.println("Start SF2");
 		loadData(O_2,LI_2);
 		System.out.println("\tStart Experiment 1");
-		System.out.println("\t\tStart 10%");
-		col = new ColSV("Col1", table, attr, lower2_10, higher2_10);
-		row = new RowSV("Row2", table, attr, lower2_10, higher2_10);
+		System.out.println("\t\tCreate ColSV for 10%");
+		ColSV col = new ColSV("Col1", table, attr, lower2_10, higher2_10);
+		System.out.println("\t\tCreate RowSV for 10%");
+		RowSV row = new RowSV("Row2", table, attr, lower2_10, higher2_10);
+		System.out.println("\t\t\tStart 10%");
 		double[] res1_3 = experiment1(col, row, table, attr, lower2_10, higher2_10, nQuery);
-		System.out.println("\t\tStart 90%");
+		System.out.println("\t\tCreate ColSV for 90%");
 		col = new ColSV("Col1", table, attr, lower2_90, higher2_90);
+		System.out.println("\t\tCreate RowSV for 90%");
 		row = new RowSV("Row2", table, attr, lower2_90, higher2_90);
+		System.out.println("\t\t\tStart 90%");
 		double[] res1_4 = experiment1(col, row, table, attr, lower2_90, higher2_90, nQuery);
 		System.out.println("\tStart Experiment 2");
+		System.out.println("\t\tCreate histogram and sketch");
 		AqpSV aqp = new AqpSV();
+		System.out.println("\t\tCreate ColSV");
 		ColSV countCol = new ColSV("Col1",table,attr,Double.MIN_VALUE,Double.MAX_VALUE);
+		System.out.println("\t\tCreate RowSV");
 		RowSV countRow = new RowSV("Row2",table,attr,Double.MIN_VALUE,Double.MAX_VALUE);
+		System.out.println("\t\t\tStart 10%");
 		double[] res2_1 = experiment2(aqp,countCol,countRow, table, attr, lower2_10, higher2_10, nQuery);
+		System.out.println("\t\t\tStart 90%");
 		double[] res2_2 = experiment2(aqp,countCol,countRow, table, attr, lower2_90, higher2_90, nQuery);
 		System.out.println("\tStart Experiment 3");
 		double[] res3 = experiment3(aqp,countCol,countRow, table, attr, Double.MIN_VALUE, Double.MAX_VALUE, nQuery);
 		
+		System.out.println("Save results");
 		ArrayList<double[]> res =new ArrayList<>();
-		res.add(res1_1);
-		res.add(res1_2);
+//		res.add(res1_1);
+//		res.add(res1_2);
 		res.add(res1_3);
 		res.add(res1_4);
 		res.add(res2_1);
 		res.add(res2_2);
 		res.add(res3);
 		
+		System.out.println("Start generating csv");
 		exportData(res);
 	}
 }
